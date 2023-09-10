92cc5de43e59761295cc5d185f1e06ce36b1ffcb
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-10 22:36:48 +0300

    Day-16 of 100

--- Day-16/README.md
+++ Day-16/README.md
@@ -1,3 +1,5 @@
 # Day-16 of 100
 
 ## Object Oriented Programming
+
+Implemented the coffee machine using OOP concepts
--- Day-16/main.py
+++ Day-16/main.py
@@ -1,7 +1,30 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import time
+
+
+my_menu = Menu()
+coffee = CoffeeMaker()
+bill = MoneyMachine()
 
 while True:
-    my_coffee = input("What do you like? (espresso/latte/cappuccino): ").lower()
-    
+    my_order = input(f"What would you like? ({my_menu.get_items()}): " ).lower()
+    if my_order == 'report':
+        coffee.report()
+    elif my_order == "quit":
+        print("Closing: ", end="")
+        for i in range(3):
+            print(".", end="")
+            time.sleep(1)
+        print()
+        break
+    else:
+        item_available = my_menu.find_drink(my_order)
+        if item_available is None:
+            continue
+    if coffee.is_resource_sufficient(item_available):
+        if bill.make_payment(item_available.cost):
+            coffee.make_coffee(item_available)
+        else:
+            continue
